ebb2b3bf6099774f24fd3416a6fc773fea10a192
Sharon Gacheru <sharongacheru2@gmail.com>
2025-04-28 10:17:32 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById('new-task-description');
+    const task = taskInput.value;
+
+    const newTask = document.createElement('li');
+    newTask.textContent = task;
+
+    const taskList = document.getElementById('tasks');
+    taskList.appendChild(newTask);
+
+    taskInput.value = '';
+  });
 });
